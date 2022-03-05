3f05aabeec054b34cbbc69b70ca57000b947758f
SebastianDevus <sebastianjogosextra@gmail.com>
2022-03-05 02:37:30 -0300

    Atividade realizada

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.Write("Preparar...");
+
+Console.ReadKey();
+Console.WriteLine(""); //Espaço entre as palavras
+Console.Write("Apontar...");
+
+Console.ReadKey();
+Console.WriteLine(""); //Espaço entre as palavras
+Console.Write("FOGO!");
